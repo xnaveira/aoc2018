d27e4631be5f96645e84e898f1efd77a60317fb7
Xavier Naveira <xnaveira@gmail.com>
2018-12-11 12:42:58 +0100

    Day 7 part 2 working

--- day7/day7.go
+++ day7/day7.go
@@ -10,13 +10,24 @@ import (
 	"sort"
 )
 
+var example bool
+
 func Run(input string) (string, string, error) {
 
 	fmt.Println("This is day7")
 
-	f, err := os.Open(input)
+	example = false
 
-	//f, err := os.Open("day7/example.txt")
+	var f *os.File
+	var err error
+	var workers int
+	if example {
+		f, err = os.Open("day7/example.txt")
+		workers = 2
+	} else {
+		f, err = os.Open(input)
+		workers = 5
+	}
 	if err != nil {
 		return "", "", err
 	}
@@ -25,6 +36,7 @@ func Run(input string) (string, string, error) {
 	scanner := bufio.NewScanner(f)
 
 	g := simple.NewDirectedGraph(0, 0)
+	gg := simple.NewDirectedGraph(0, 0)
 
 	for scanner.Scan() {
 		var s1, s2 string
@@ -37,7 +49,14 @@ func Run(input string) (string, string, error) {
 		}
 
 		fmt.Println(s1, " ", s2)
-		g.SetEdge(simple.Edge{simple.Node(s1[0]), simple.Node(s2[0]), 0})
+		g.SetEdge(simple.Edge{
+			simple.Node(formatInput(int(s1[0]), example)),
+			simple.Node(formatInput(int(s2[0]), example)),
+			0})
+		gg.SetEdge(simple.Edge{
+			simple.Node(formatInput(int(s1[0]), example)),
+			simple.Node(formatInput(int(s2[0]), example)),
+			0})
 	}
 
 	transitiveReduction(g)
@@ -56,10 +75,17 @@ func Run(input string) (string, string, error) {
 
 	result1 := ""
 	for _, e := range sorted {
-		result1 = result1 + string(e.ID())
+		result1 = result1 + string(formaOutput(e.ID(), example))
 	}
 
-	return result1, "", nil
+	//transitiveReduction(gg)
+
+	steps, err := kahnWorkers(gg, workers)
+	if err != nil {
+		return "", "", err
+	}
+
+	return result1, string(steps), nil
 }
 
 func transitiveReduction(g *simple.DirectedGraph) {
@@ -74,6 +100,89 @@ func transitiveReduction(g *simple.DirectedGraph) {
 			}
 		}
 	}
+}
+
+func kahnWorkers(g *simple.DirectedGraph, w int) (int, error) {
+	S := []graph.Node{}
+	seconds := -1
+	type worker struct {
+		n graph.Node
+		u int
+	}
+	working := []worker{}
+	visited := map[graph.Node]bool{}
+	for _, n := range g.Nodes() {
+		if len(g.To(n)) == 0 {
+			S = append(S, n)
+		}
+	}
+	for {
+		if len(S) == 0 && len(working) == 0 {
+			break
+		}
+		seconds++
+		tmpS := []graph.Node{}
+		for _, m := range S {
+			if len(working) < w {
+				if !visited[m] {
+					parents := g.To(m)
+					parentsInWorking := false
+					for _, w := range working {
+						for _, p := range parents {
+							if w.n == p {
+								parentsInWorking = true
+							}
+						}
+					}
+					numOfParents := len(parents)
+					visitedParents := 0
+					for k, v := range visited {
+						for _, p := range parents {
+							if v == true && k == p {
+								visitedParents++
+							}
+						}
+					}
+					parentsInVisited := numOfParents == visitedParents
+					if !parentsInWorking && parentsInVisited {
+						working = append(working, worker{m, m.ID()})
+						sort.SliceStable(working, func(i, j int) bool {
+							return working[i].n.ID() < working[j].n.ID()
+						})
+						visited[m] = true
+					}
+				}
+			} else {
+				tmpS = append(tmpS, m)
+			}
+		}
+		S = tmpS
+		tmpworking := []worker{}
+		for i, ww := range working {
+			fmt.Printf("Second: %d. Working on: ", seconds)
+			for _, w := range working {
+				fmt.Printf("%s %d ", string(formaOutput(w.n.ID(), example)), w.u)
+			}
+			fmt.Printf("\n")
+			working[i].u--
+			if working[i].u == 0 {
+				S = append(S, g.From(ww.n)...)
+				sort.SliceStable(S, func(i, j int) bool {
+					return S[i].ID() < S[j].ID()
+				})
+			} else {
+				tmpworking = append(tmpworking, working[i])
+			}
+		}
+		working = tmpworking
+
+	}
+	if len(g.Edges()) != 0 {
+		return 0, fmt.Errorf("the graph ahs at least one cycle: %v", g.Edges())
+	} else {
+		return seconds, nil
+	}
+
 }
 
 func kahn(g *simple.DirectedGraph) ([]graph.Node, error) {
@@ -109,4 +218,20 @@ func kahn(g *simple.DirectedGraph) ([]graph.Node, error) {
 		return L, nil
 	}
 
+}
+
+func formatInput(i int, e bool) int {
+	if e {
+		return i - 64
+	} else {
+		return i - 64 + 60
+	}
+}
+
+func formaOutput(i int, e bool) int {
+	if e {
+		return i + 64
+	} else {
+		return i + 64 - 60
+	}
 }
